5d4f6dc1d90480459caadc2196bea39de9e0c441
Saverio <95577971+saverioscagnoli@users.noreply.github.com>
2023-05-15 18:08:51 +0200

    remove testing cd

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -4,7 +4,6 @@ import { CHIPS_EMOJI, Utils } from "../utils";
 export default new SlashCommand({
   name: "balance",
   description: "Check your balance and stats.",
-  cd: 6e4,
   exe: async ({ int, data }) => {
     let { chips, wins, losses } = data;
     let ebd = Utils.embed({
